3c8eec9a1f4f71e00d3da9ecf77a702af2f63675
26432 <2643298080@qq.com>
2026-04-16 15:03:14 +0800

    fix: 修复部署路径问题

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,23 +2,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [vue()],  // 启用Vue插件
+    plugins: [vue()],
+    base: './',  // <--- 👈 加上这一行，强制使用相对路径
     server: {
-        port: 5173,  // 前端开发端口（避免与后端8080冲突）
-        open: true,  // 启动后自动打开浏览器
-        host: '0.0.0.0',  // 允许局域网访问
-        // 跨域代理（解决前端直接访问后端接口的跨域问题，无需修改后端配置）
+        port: 5173,
+        open: true,
+        host: '0.0.0.0',
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',  // 后端接口地址
-                changeOrigin: true,  // 允许跨域
-                //rewrite: (path) => path.replace(/^\/api/, '')  // 重写路径，去掉/api前缀
+                target: 'http://localhost:8080',
+                changeOrigin: true,
             }
         }
     },
     resolve: {
         alias: {
-            '@': '/src'  // 别名配置，方便引入src目录下的文件（如@/components）
+            '@': '/src'
         }
     }
 })
