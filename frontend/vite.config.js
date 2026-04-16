343437c7beac75ddf9be47988595f60f1f27378f
26432 <2643298080@qq.com>
2026-04-16 15:27:34 +0800

    fix: 让 Vite 自动处理脚本引入

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [vue()],
-    base: './',  // <--- 👈 加上这一行，强制使用相对路径
+    base: '/',  // <--- 👈 加上这一行，强制使用相对路径
     server: {
         port: 5173,
         open: true,
